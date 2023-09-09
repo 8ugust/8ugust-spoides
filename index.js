ea07640173b7646eac661196cf046b17cd1d1f3e
shmin <gks831@naver.com>
2023-09-10 01:16:30 +0900

    Create Table Change Function

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 // ==================== ==================== ==================== ====================
 // Declare Global Variation
 // ==================== ==================== ==================== ====================
+let loading;
 let level;
+
 
 
 
@@ -12,6 +14,8 @@ let level;
 // ==================== ==================== ==================== ====================
 document.addEventListener("DOMContentLoaded", () => {
     level = document.getElementById('level').innerHTML;
+    loading = document.getElementById('loading-wrap').style;
+    fnCreateTable();
 })
 
 
@@ -23,18 +27,18 @@ document.addEventListener("DOMContentLoaded", () => {
 // ==================== ==================== ==================== ====================
 const fnChangeLevel = (mode) => {
 
-    // =============== =============== =============== ===============
+    // =============== =============== =============== 
     // Return.
-    // =============== =============== =============== ===============
+    // =============== =============== =============== 
     const upBtn = document.getElementById('levelUp');
     const dnBtn = document.getElementById('levelDn');
     if (mode == 'U' && level == 5 || mode == 'D' && level == 1) {
         return false; 
     }
 
-    // =============== =============== =============== ===============
+    // =============== =============== =============== 
     // Level & CSS Change.
-    // =============== =============== =============== ===============
+    // =============== =============== =============== 
     if (mode == 'U') level++;
     if (mode == 'D') level--;
     document.getElementById('level').innerHTML = level;
@@ -51,5 +55,46 @@ const fnChangeLevel = (mode) => {
         upBtn.style.cursor = 'pointer';
         dnBtn.style.cursor = 'pointer';
     }
+
+    // =============== =============== =============== 
+    // Create Table
+    // =============== =============== =============== 
+    fnCreateTable();
+}
+
+
+
+
+
+// ==================== ==================== ==================== ====================
+// Create Table Area
+// ==================== ==================== ==================== ====================
+const fnCreateTable = () => {
+    const target = document.getElementById('tableTarget');
+    let count; target.innerHTML = '';
+    if (level == 1) count = 3;
+    if (level == 2) count = 4;
+    if (level == 3) count = 5;
+    if (level == 4) count = 6;
+    if (level == 5) count = 7;
+
+    loading.display = 'flex';
+    for (let i = 0; i < count; i++) {
+        const tableR = document.createElement('tr');
+        for (let j = 0; j < count; j++) {
+            const tableV = document.createElement('div');
+            const tableD = document.createElement('td');
+            tableV.style.justifyContent = 'center';
+            tableV.style.alignItems = 'center';
+            tableV.style.display = 'flex';
+            tableV.innerHTML = 'Hi';
+
+            tableD.appendChild(tableV);
+            tableR.appendChild(tableD);
+        }
+
+        target.appendChild(tableR);
+    }
     
+    loading.display = 'none';
 }
